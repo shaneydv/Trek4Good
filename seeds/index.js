bd73e079de0e1048c3067a409dbfb6ea119beb38
Sushane <sushaneyadav@gmail.com>
2022-02-14 23:17:57 +0530

    Finish basic styles and layouts bootstrap

--- seeds/index.js
+++ seeds/index.js
@@ -27,7 +27,7 @@ const seedDB = async () => {
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://unsplash.com/collections/483251/in-the-woods',
+            image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price
         })
